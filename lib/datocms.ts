fa1dcdd5c139203372cb9af499e309e213190efe
Michael Sampietro <mssampietro@gmail.com>
2022-01-15 01:25:03 -0300

    begin datocms consumption

---
+++ lib/datocms.ts
@@ -0,0 +1,12 @@
+import { GraphQLClient } from "graphql-request";
+
+export function request({ query, variables }: any) {
+  const endpoint = `https://graphql.datocms.com/`;
+  const client = new GraphQLClient(endpoint, {
+    headers: {
+      // authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
+      authorization: `Bearer 2d1770a4d2f741c8d8a7c3d6730d8b`,
+    },
+  });
+  return client.request(query, variables);
+}
